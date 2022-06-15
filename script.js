5c6515b701a099a9bdecf35bff8a791fbd30c6bc
bakhtyar25 <bakhtyarmhamad12@gmail.com>
2022-06-15 22:35:13 +0300

    random editing

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
 const btn= document.querySelector('.darkmode');
-const darkakan= document.querySelector('.bg-dark')
+const darkakan= document.querySelectorAll('.bg-dark')
 const textdarkakan= document.querySelectorAll('.text-light')
-
-
-// btn.style. = "red";
-
+const icon=document.querySelector('.darkicon')
 
 let a=0
 function darkMode(){
     ++a;
     if(a%2!=0){
        
-        darkakan.style.setProperty('background-color', '#f8f9fa', 'important');       
-        // textdarkakan.style.setProperty('color', '#212529', 'important');       
+               
         for (let i = 0; i < textdarkakan.length; i++) {
             textdarkakan[i].style.setProperty('color', '#212529', 'important');
           }
-        
+        for (let i = 0; i < darkakan.length; i++) {
+            darkakan[i].style.setProperty('background-color', '#f8f9fa', 'important');
+          }
+        icon.setAttribute('src','images/dark.png');
+
     }else if(a%2==0){
         
-        darkakan.style.setProperty('background-color', '#212529', 'important');       
-        // textdarkakan.style.setProperty('color', '#f8f9fa', 'important'); 
         for (let i = 0; i < textdarkakan.length; i++) {
             textdarkakan[i].style.setProperty('color', '#f8f9fa', 'important');
           }
-        
-        
+        for (let i = 0; i < darkakan.length; i++) {
+            darkakan[i].style.setProperty('background-color', '#212529', 'important');
+          }
+        icon.setAttribute('src','images/light.png');   
     }
 }
-
 btn.addEventListener('click',darkMode);
