09f90b708a1b03bcb50e126d4857761026d76f3d
bakhtyar25 <bakhtyarmhamad12@gmail.com>
2022-06-16 22:07:33 +0300

    creating my hobbies and carrer section with scroll animation

--- public/script.js
+++ public/script.js
@@ -1,6 +1,8 @@
 const btn= document.querySelector('.darkmode');
 const darkakan= document.querySelectorAll('.bg-dark')
-const textdarkakan= document.querySelectorAll('.text-light')
+const textalightakan= document.querySelectorAll('.text-light')
+const textdarkakan= document.querySelectorAll('.text-dark')
+const ronakakan= document.querySelectorAll('.bg-light')
 const icon=document.querySelector('.darkicon')
 
 let a=0
@@ -9,22 +11,41 @@ function darkMode(){
     if(a%2!=0){
        
                
-        for (let i = 0; i < textdarkakan.length; i++) {
-            textdarkakan[i].style.setProperty('color', '#212529', 'important');
+        for (let i = 0; i < textalightakan.length; i++) {
+            textalightakan[i].style.setProperty('color', '#212529', 'important');
           }
+        for (let i = 0; i < textdarkakan.length; i++) {
+            textdarkakan[i].style.setProperty('color', '#f8f9fa', 'important');
+          }
+
         for (let i = 0; i < darkakan.length; i++) {
             darkakan[i].style.setProperty('background-color', '#f8f9fa', 'important');
           }
+
+        for (let i = 0; i < darkakan.length; i++) {
+            ronakakan[i].style.setProperty('background-color', '#212529', 'important');
+          }
+
         icon.setAttribute('src','../images/dark.png');
 
     }else if(a%2==0){
         
-        for (let i = 0; i < textdarkakan.length; i++) {
-            textdarkakan[i].style.setProperty('color', '#f8f9fa', 'important');
+        for (let i = 0; i < textalightakan.length; i++) {
+            textalightakan[i].style.setProperty('color', '#f8f9fa', 'important');
           }
+
+        for (let i = 0; i < textdarkakan.length; i++) {
+            textdarkakan[i].style.setProperty('color', '#212529', 'important');
+          }
+
         for (let i = 0; i < darkakan.length; i++) {
             darkakan[i].style.setProperty('background-color', '#212529', 'important');
           }
+
+        for (let i = 0; i < darkakan.length; i++) {
+            ronakakan[i].style.setProperty('background-color', '#f8f9fa', 'important');
+          }
+
         icon.setAttribute('src','../images/light.png');   
     }
 }
@@ -54,12 +75,30 @@ image.addEventListener('mouseout',function(){
 
 
 
-// let fff={
-//   emp:[
-//     {firsname:'baxa', last:'nn'},
-//     {firsname:'baxa', last:'nn'}
-//   ]
-// }
+var items = document.querySelectorAll(".timeline li");
 
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
 
-// foreach()
+function callbackFunc() {
+  for (var i = 0; i < items.length; i++) {
+    if (isElementInViewport(items[i])) {
+      if(!items[i].classList.contains("in-view")){
+        items[i].classList.add("in-view");
+      }
+    } else if(items[i].classList.contains("in-view")) {
+        items[i].classList.remove("in-view");
+    }
+  }
+}
+ 
+window.addEventListener("load", callbackFunc);
+window.addEventListener("scroll", callbackFunc);
+
