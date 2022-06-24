cda468c27ae7f8abdbbe5395dc90dcbe43702d55
bakhtyar25 <bakhtyarmhamad12@gmail.com>
2022-06-24 19:18:14 +0300

    adding icon to the tab and working on scroll events for main page

---
+++ public/test.js
@@ -0,0 +1,13 @@
+const divs = document.querySelector('body');
+
+divs.onscroll=gg
+
+function gg() {
+    divs.style.color = 'teal'
+    return false;
+}
+
+
+// window.onscroll = function () {
+//         gg();
+//       };
