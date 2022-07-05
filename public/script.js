9e763f6108b02b0e43cb6ffb3573f2161f2e2019
bakhtyar25 <bakhtyarmhamad12@gmail.com>
2022-07-05 16:52:35 +0300

    scroll editing

--- public/script.js
+++ public/script.js
@@ -120,16 +120,9 @@ function scrollFunction() {
 // When the user clicks on the button, scroll to the top of the document
 mybutton.addEventListener("click", backToTop);
 
-
 function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
-
-    // window.scrollTo({
-    //   top: 0,
-    //   behavior: 'smooth'
-    // });
-
 }
 
 
