7cd5b3db6c3463e2436dd2c20b8094ab9205cf08
bakhtyar25 <bakhtyarmhamad12@gmail.com>
2022-07-06 14:47:43 +0300

    scroll editing6

--- public/script.js
+++ public/script.js
@@ -121,8 +121,14 @@ function scrollFunction() {
 mybutton.addEventListener("click", backToTop);
 
 function backToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  // document.body.scrollTop = 0;
+  // document.documentElement.scrollTop = 0;
+
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+
 }
 
 
